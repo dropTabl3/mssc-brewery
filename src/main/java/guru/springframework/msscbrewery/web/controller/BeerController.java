52a8cefbb55a6a5ac1345eb13b7b61d58175f76d
DESKTOP-FPGNPAL\Massimo <mxmo.barbero@gmail.com>
2023-08-29 23:11:45 +0200

    clean comment

--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -27,7 +27,7 @@ public class BeerController {
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
     }
 
-    @PostMapping // POST - create new beer
+    @PostMapping
     public ResponseEntity handlePost(@RequestBody BeerDto beerDto) {
         BeerDto savedDto = beerService.saveNewBeer(beerDto);
         HttpHeaders headers = new HttpHeaders();
