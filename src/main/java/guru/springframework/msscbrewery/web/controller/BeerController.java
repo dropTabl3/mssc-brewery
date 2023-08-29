d8b5d6c72c7cc0941c18197048b809e3c14eccd5
DESKTOP-FPGNPAL\Massimo <mxmo.barbero@gmail.com>
2023-08-29 22:02:07 +0200

    Put lesson

--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -29,7 +29,7 @@ public class BeerController {
     }
 
     @PostMapping // POST - create new beer
-    public ResponseEntity handlePost(BeerDto beerDto){
+    public ResponseEntity handlePost(@RequestBody BeerDto beerDto){
 
         BeerDto savedDto = beerService.saveNewBeer(beerDto);
 
@@ -38,6 +38,15 @@ public class BeerController {
         headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
 
         return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping({"/{beerId}"})
+    public ResponseEntity handlePut(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+
+        beerService.updateBeer(beerId,beerDto);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+
     }
 
 }
