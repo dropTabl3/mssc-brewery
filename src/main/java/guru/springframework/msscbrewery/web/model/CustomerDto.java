78ff8544ffc7498229806fed945e2c8ec2d9eb0c
Massimo Barbero <mxmo.barbero@gmail.com>
2023-09-01 21:12:52 +0200

    assignment complete

--- src/main/java/guru/springframework/msscbrewery/web/model/CustomerDto.java
+++ src/main/java/guru/springframework/msscbrewery/web/model/CustomerDto.java
@@ -4,7 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Data
@@ -14,6 +16,9 @@ import java.util.UUID;
 public class CustomerDto {
 
     private UUID id;
+
+    @NotNull
+    @Length(min = 3, max = 100)
     private String customerName;
 
 }
